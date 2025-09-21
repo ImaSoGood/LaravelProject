360dc3cfd73bb6870ee11b488f76f3e3192534ea
Volodya <vova_kovalev_5667@mail.ru>
2025-09-21 10:41:09 +0300

    существлен перенос с  SQLite на MySQL. ункция регистронезависимого поиска - работает! вместо генерации иностранных имен, создана генерация на основе русских имен, фамилий и отчеств

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -14,8 +14,14 @@ class AuthorFactory extends Factory
 
     public function definition(): array
     {
+        $firstName = ['Владимир', 'Алексей', 'Сергей', 'Дмитрий', 'Эльдар', 'Александр', 'Данин', 'Аркадий', 'Марк', 'Вячеслав', 'Андрей'];
+        $lastName = ['Ковалев', 'Ким', 'Извеков', 'Сотников', 'Подлужный', 'Картавый', 'Ненашев', 'Гацуленко', 'Толстой', 'Кузнецов', 'Брагин'];
+        $patronymic = ['Владимирович', 'Алексеевич', 'Сергеевич', 'Дмитриевич', 'Эльдарович', 'Александрович', 'Данинович', 'Аркадьевич', 'Маркович', 'Вячеславович', 'Андреевич'];
+
+        $fullName = $this->faker->randomElement($lastName). ' ' . $this->faker->randomElement($firstName). ' ' . $this->faker->randomElement($patronymic);
+
         return [
-            'full_name' => $this->faker->name(),
+            'full_name' => $fullName
         ];
     }
 }
